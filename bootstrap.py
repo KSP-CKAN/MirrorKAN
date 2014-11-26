a02ffa671f91acf807983ffc97f88c39bee33f24
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-26 10:37:44 +0200

    More auto generating goodness

---
+++ bootstrap.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import os, sys
+
+def make_config_file(config_path, master_repo, master_root, lokal_ckan, mirror_path, local_url, index_header):
+    cfg = """MASTER_REPO = '%s'
+MASTER_ROOT_PATH = '%s'
+LOCAL_CKAN_PATH = '%s'
+FILE_MIRROR_PATH = '%s'
+LOCAL_URL_PREFIX = '%s'
+INDEX_HTML_HEADER = '%s'""" % (master_repo, master_root, lokal_ckan, mirror_path, local_url, index_header)
+
+    with open(config_path, 'w') as config_file:
+        config_file.write(cfg)
+
+def ask_user(message, default):
+	user = ''
+	
+	if default != None:
+		user = raw_input('%s [%s]: ' % (message, default))
+	else:
+		user = raw_input('%s: ' % message)
+		
+	if user == '':
+		user = default
+	
+	return user
+
+def main():
+    print 'Bootstrapping MirrorKAN in the current working directory'
+    
+    os.system('wget https://github.com/KSP-CKAN/MirrorKAN/archive/master.zip')
+    os.system('unzip -o master.zip')
+    os.system('rm master.zip')
+    os.system('mv MirrorKAN-master MirrorKAN')
+    
+    print 'Preparing configuration'
+    
+    master_repo = ask_user("Set the CKAN master repository to mirror (url to master.zip)", "https://github.com/KSP-CKAN/CKAN-meta/archive/master.zip")
+    
+    root = os.getcwd()
+    print 'Root: ' + root
+    
+    master_root = os.path.join(root, 'master')
+    if not os.path.exists(master_root):
+		os.makedirs(master_root)
+    
+    local_ckan = os.path.join(root, 'local_ckan')
+    
+    if not os.path.exists(local_ckan):
+		os.makedirs(local_ckan)
+    
+    mirror_path = ask_user("Set the local path where downloads will be mirrored (must be visible on the web)", None)
+    local_url = ask_user("Set the URL pointing to the mirror path", None)
+    index_header = ask_user("Enter a description for this repo", None)
+    
+    print 'Writing mirrorkan_conf.py..',
+    config_path = os.path.join(os.path.join(root, "MirrorKAN"), "mirrorkan_conf.py")
+    make_config_file(config_path, master_repo, master_root, local_ckan, mirror_path, local_url, index_header)
+    print 'Done!'
+    
+    print 'Generating shell scripts..'
+    generate_scripts_path = os.path.join(os.path.join(root, "MirrorKAN"), "generate_scripts.py")
+    log_path = os.path.join(mirror_path, 'log.txt')
+    os.system('python ' + generate_scripts_path + ' "' + os.path.join(root, 'all.sh') + '" "' + log_path + '" "' + root + '" "' + mirror_path + '"')
+    print 'Done!'
+    
+if __name__ == "__main__":
+    main()
--- generate_scripts.py
+++ generate_scripts.py
@@ -3,46 +3,90 @@
 import os, sys
 
 class Script:
-	def __init__(self):
-		self.text = ''
-		
-	def append(self, text):
-		if text.split(' ')[0] != 'echo':
-			self.text += 'echo "Executing: %s"\n' % text.split('\n')[0]
-		self.text += text
+    def __init__(self):
+        self.text = ''
+        
+    def append(self, text):
+        if text.split(' ')[0] != 'echo':
+            self.text += 'echo "Executing: %s" | $tee\n' % text.split('\n')[0].split('|')[0].replace('"', '')
+        self.text += text
+      
+    def append_nolog(self, text):
+        self.text += text
+
+def append_clean_up(script, log_path):
+    script.append('rm %s\n' % log_path)
 
 def append_tee_header(script, log_path):
-	script.append("tee='tee -a %s'\n" % log_path)
-	
+    script.append("tee='tee -a %s'\n" % log_path)
+    
 def append_clone_repo(script, repo):
-	script.append("git clone %s 2>&1 | $tee\n" % repo)
-	
+    script.append("git clone %s 2>&1 | $tee\n" % repo)
+    
 def append_update_mirrorkan(script, mirrorkan_root):
-	script.append("cd %s\n" % mirrorkan_root)
-	append_clone_repo(script, "https://github.com/KSP-CKAN/MirrorKAN.git")
-	script.append("cd MirrorKAN\n")
-	script.append("python mirrorkan.py | $tee")
+    script.append("cd %s\n" % mirrorkan_root)
+    
+    append_clone_repo(script, "https://github.com/KSP-CKAN/MirrorKAN.git")
+    script.append("cd MirrorKAN\n")
+    script.append("python mirrorkan.py | $tee")
 
-def append_ckan_build(script):
-	script.append("rm CKAN -R\n")
-	append_clone_repo(script, "https://github.com/KSP-CKAN/CKAN.git")
-	script.append("cd CKAN\n")
-	script.append("perl bin/build | $tee\n")
+def append_ckan_build(script, mirrorkan_root):
+    script.append("cd %s\n" % mirrorkan_root)
+    
+    script.append("rm CKAN -R\n")
+    append_clone_repo(script, "https://github.com/KSP-CKAN/CKAN.git")
+    script.append("cd CKAN\n")
+    script.append("perl bin/build | $tee\n")
+
+def append_update_ckan_meta(script, mirrorkan_root):
+    script.append("cd %s\n" % mirrorkan_root)
+    script.append("rm CKAN-meta -R\n")
+    append_clone_repo(script, "https://github.com/KSP-CKAN/CKAN-meta.git")
+    
+def append_update_netkan(script, mirrorkan_root, mirrorkan_cache):
+    script.append("cd %s\n" % mirrorkan_root)
+    
+    netkan_exe_path = os.path.join(os.path.join(mirrorkan_root, "CKAN"), "netkan.exe")
+    netkans_path = os.path.join(os.path.join(mirrorkan_root, "NetKAN"), "NetKAN")
+    output_path = os.path.join(mirrorkan_root, "CKAN-meta")
+    
+    script.append("rm NetKAN -R\n")
+    append_clone_repo(script, "https://github.com/KSP-CKAN/NetKAN.git")
+    script.append("cd NetKAN\n")
+    script.append("cd NetKAN\n")
+    script.append_nolog("for f in *.netkan\n")
+    script.append_nolog("do\n")
+    script.append("%s %s/$f --cachedir=%s --outputdir=%s | $tee\n" % (netkan_exe_path, netkans_path, mirrorkan_cache, output_path))
+    script.append_nolog("done\n")
+
+def append_push_ckan_meta(script, mirrorkan_root):
+    script.append("cd %s\n" % mirrorkan_root)
+    script.append("cd CKAN-meta\n")
+    script.append("git add * 2>&1 | $tee\n")
+    script.append("git commit -m \"NetKAN generated mods\" 2>&1 | $tee\n")
+    script.append("git push 2>&1 | $tee\n")
 
 def main():
-	if len(sys.argv) < 3:
-		print 'Usage:'
-		print sys.argv[0] + ' <target bash script> <log path> <mirrorkan root>'
-		sys.exit(0)
-		
-	script = Script()
-	append_tee_header(script, sys.argv[2])
-	
-	append_ckan_build(script)
-	append_update_mirrorkan(script, sys.argv[3])
-		
-	with open(sys.argv[1], 'w') as script_file:
-		script_file.write(script.text)
+    if len(sys.argv) < 4:
+        print 'Usage:'
+        print sys.argv[0] + ' <target bash script> <log path> <mirrorkan root> <mirrorkan cache>'
+        sys.exit(0)
+    
+    log_path = sys.argv[2]     
+    mirrorkan_root = sys.argv[3]
+    mirrorkan_cache = sys.argv[4]
+
+    script = Script()
+    append_tee_header(script, log_path)
+    append_clean_up(script, log_path)
+    append_ckan_build(script, mirrorkan_root)
+    append_update_ckan_meta(script, mirrorkan_root)
+    append_update_netkan(script, mirrorkan_root, mirrorkan_cache)
+    append_push_ckan_meta(script, mirrorkan_root)
+    append_update_mirrorkan(script, mirrorkan_root)
+        
+    with open(sys.argv[1], 'w') as script_file:
+        script_file.write(script.text)
 
 if __name__ == "__main__":
-	main()
+    main()
