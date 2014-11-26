c3ccef02e327a554b1c02ae99b0b2423a62d29a5
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-26 13:02:25 +0200

    don't regenerate config if it already exists

--- bootstrap.py
+++ bootstrap.py
@@ -14,17 +14,17 @@ INDEX_HTML_HEADER = '%s'""" % (master_repo, master_root, lokal_ckan, mirror_path
         config_file.write(cfg)
 
 def ask_user(message, default):
-	user = ''
-	
-	if default != None:
-		user = raw_input('%s [%s]: ' % (message, default))
-	else:
-		user = raw_input('%s: ' % message)
-		
-	if user == '':
-		user = default
-	
-	return user
+    user = ''
+    
+    if default != None:
+        user = raw_input('%s [%s]: ' % (message, default))
+    else:
+        user = raw_input('%s: ' % message)
+        
+    if user == '':
+        user = default
+    
+    return user
 
 def main():
     print 'Bootstrapping MirrorKAN in the current working directory'
@@ -41,23 +41,25 @@ def main():
     root = os.getcwd()
     print 'Root: ' + root
     
-    master_root = os.path.join(root, 'master')
-    if not os.path.exists(master_root):
-		os.makedirs(master_root)
-    
-    local_ckan = os.path.join(root, 'local_ckan')
-    
-    if not os.path.exists(local_ckan):
-		os.makedirs(local_ckan)
-    
-    mirror_path = ask_user("Set the local path where downloads will be mirrored (must be visible on the web)", None)
-    local_url = ask_user("Set the URL pointing to the mirror path", None)
-    index_header = ask_user("Enter a description for this repo", None)
-    
-    print 'Writing mirrorkan_conf.py..',
     config_path = os.path.join(os.path.join(root, "MirrorKAN"), "mirrorkan_conf.py")
-    make_config_file(config_path, master_repo, master_root, local_ckan, mirror_path, local_url, index_header)
-    print 'Done!'
+    
+    if not os.path.exists(config_path):
+        master_root = os.path.join(root, 'master')
+        if not os.path.exists(master_root):
+            os.makedirs(master_root)
+        
+        local_ckan = os.path.join(root, 'local_ckan')
+        
+        if not os.path.exists(local_ckan):
+            os.makedirs(local_ckan)
+        
+        mirror_path = ask_user("Set the local path where downloads will be mirrored (must be visible on the web)", None)
+        local_url = ask_user("Set the URL pointing to the mirror path", None)
+        index_header = ask_user("Enter a description for this repo", None)
+        
+        print 'Writing mirrorkan_conf.py..',
+        make_config_file(config_path, master_repo, master_root, local_ckan, mirror_path, local_url, index_header)
+        print 'Done!'
     
     print 'Generating shell scripts..'
     generate_scripts_path = os.path.join(os.path.join(root, "MirrorKAN"), "generate_scripts.py")
