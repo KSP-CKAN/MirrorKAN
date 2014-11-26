2382e3b487ceb279bda97f86f33b38c0e8b6c05f
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-26 19:31:50 +0200

    fix

--- generate_scripts.py
+++ generate_scripts.py
@@ -12,70 +12,70 @@ class Script:
     def append(self, text):
         if text.split(' ')[0] != 'echo':
             self.text += 'echo "Executing: %s" | $tee\n' % text.split('\n')[0].split('|')[0].replace('"', '')
-        self.text += text
+        self.text += text + '\n'
       
     def append_nolog(self, text):
-        self.text += text
+        self.text += text + '\n'
 
 def append_clean_up(script, log_path):
-    script.append('rm %s\n' % log_path)
+    script.append('rm %s' % log_path)
 
 def append_tee_header(script, log_path):
-    script.append("tee='tee -a %s'\n" % log_path)
+    script.append("tee='tee -a %s'" % log_path)
     
 def append_clone_repo(script, repo):
-    script.append("git clone %s 2>&1 | $tee\n" % repo)
+    script.append("git clone %s 2>&1 | $tee" % repo)
     
 def append_update_mirrorkan(script, mirrorkan_root):
-    script.append("cd %s\n" % mirrorkan_root)
-    script.append("cd MirrorKAN\n")
-    script.append("python mirrorkan.py | $tee\n")
+    script.append("cd %s" % mirrorkan_root)
+    script.append("cd MirrorKAN")
+    script.append("python mirrorkan.py | $tee")
 
 def append_ckan_build(script, mirrorkan_root, mirrorkan_cache):
-    script.append("cd %s\n" % mirrorkan_root)
+    script.append("cd %s" % mirrorkan_root)
     
-    script.append("rm CKAN -R\n")
+    script.append("rm CKAN -R")
     append_clone_repo(script, "https://github.com/KSP-CKAN/CKAN.git")
-    script.append("cd CKAN\n")
-    script.append("perl bin/build | $tee\n")
-    script.append("cp %s %s\n" % ("ckan.exe", mirrorkan_cache))
-    script.append("cp %s %s\n" % ("netkan.exe", mirrorkan_cache))
+    script.append("cd CKAN")
+    script.append("perl bin/build | $tee")
+    script.append("cp %s %s" % ("ckan.exe", mirrorkan_cache))
+    script.append("cp %s %s" % ("netkan.exe", mirrorkan_cache))
 
 def append_update_ckan_meta(script, mirrorkan_root):
-    script.append("cd %s\n" % mirrorkan_root)
-    script.append("rm CKAN-meta -R\n")
+    script.append("cd %s" % mirrorkan_root)
+    script.append("rm CKAN-meta -R")
     append_clone_repo(script, "https://github.com/KSP-CKAN/CKAN-meta.git")
     
 def append_update_netkan(script, mirrorkan_root, mirrorkan_cache):
-    script.append("cd %s\n" % mirrorkan_root)
+    script.append("cd %s" % mirrorkan_root)
     
     netkan_exe_path = os.path.join(os.path.join(mirrorkan_root, "CKAN"), "netkan.exe")
     netkans_path = os.path.join(os.path.join(mirrorkan_root, "NetKAN"), "NetKAN")
     output_path = os.path.join(mirrorkan_root, "CKAN-meta")
     
-    script.append("rm NetKAN -R\n")
+    script.append("rm NetKAN -R")
     append_clone_repo(script, "https://github.com/KSP-CKAN/NetKAN.git")
-    script.append("cd NetKAN\n")
-    script.append("cd NetKAN\n")
-    script.append_nolog("for f in *.netkan\n")
-    script.append_nolog("do\n")
-    script.append("%s %s/$f --cachedir=%s --outputdir=%s | $tee\n" % (netkan_exe_path, netkans_path, mirrorkan_cache, output_path))
-    script.append_nolog("done\n")
+    script.append("cd NetKAN")
+    script.append("cd NetKAN")
+    script.append_nolog("for f in *.netkan")
+    script.append_nolog("do")
+    script.append("%s %s/$f --cachedir=%s --outputdir=%s | $tee" % (netkan_exe_path, netkans_path, mirrorkan_cache, output_path))
+    script.append_nolog("done")
 
 def append_push_ckan_meta(script, mirrorkan_root):
-    script.append("cd %s\n" % mirrorkan_root)
-    script.append("cd CKAN-meta\n")
-    script.append("git add * 2>&1 | $tee\n")
-    script.append("git commit -m \"NetKAN generated mods\" 2>&1 | $tee\n")
-    script.append("git push 2>&1 | $tee\n")
+    script.append("cd %s" % mirrorkan_root)
+    script.append("cd CKAN-meta")
+    script.append("git add * 2>&1 | $tee")
+    script.append("git commit -m \"NetKAN generated mods\" 2>&1 | $tee")
+    script.append("git push 2>&1 | $tee")
 
 def append_parse_events(script, mirrorkan_root, log_path):
-    script.append("cd %s\n" % os.path.join(mirrorkan_root, "MirrorKAN"))
-    script.append("python mirrorkan_parse_events.py %s MirrorKAN/log.json | $tee\n" % log_path)
+    script.append("cd %s" % os.path.join(mirrorkan_root, "MirrorKAN"))
+    script.append("python mirrorkan_parse_events.py %s MirrorKAN/log.json | $tee" % log_path)
 
 def append_generate_index(script, mirrorkan_root, mirrorkan_cache):
     script.append("cd %s\n" % os.path.join(mirrorkan_root, "MirrorKAN"))
-    script.append("python mirrorkan_generate_index.py | $tee\n")
+    script.append("python mirrorkan_generate_index.py | $tee")
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
