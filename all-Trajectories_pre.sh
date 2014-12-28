35cdd36f853e65a10af09025ec0d5a1eb5f8a8e1
Hakan Tandogan <hakan@gurkensalat.com>
2014-12-28 10:46:06 +0100

    Launcher scripts for Trajectories

---
+++ all-Trajectories_pre.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+. /opt/mono/env.sh
+
+HERE=/home/ckan
+
+
+mkdir -p ${HERE}/CKAN
+
+cp ${HERE}/Dropbox/ckan/*.exe ${HERE}/CKAN
+
+if [ -d ${HERE}/MirrorKAN.x ]
+then
+    rm -f ${HERE}/MirrorKAN.x/mirrorkan_conf.py
+    ln -sf ${HERE}/mirrorkan_conf_Trajectories_pre.py ${HERE}/MirrorKAN.x/mirrorkan_conf.py
+fi
+
+
+THINGS=""
+THINGS="${THINGS} --clean"
+THINGS="${THINGS} --update-netkan"
+THINGS="${THINGS} --push-ckan-meta"
+THINGS="${THINGS} --update-mirrorkan"
+THINGS="${THINGS} --generate-index"
+THINGS="${THINGS} --generate-api"
+THINGS="${THINGS} --generate-feed"
+THINGS="${THINGS} "
+
+python ${HERE}/MirrorKAN.x/generate_scripts.py ${THINGS} | sh
