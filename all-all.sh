db2ecee61244fe8184799572c240591cb2910501
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-03 18:29:04 +0100

    Index Pre-releases of RemoteTech too

--- all-all.sh
+++ all-all.sh
@@ -2,6 +2,7 @@
 
 ${HOME}/MirrorKAN.munich/fetch-netkan-exe.sh
 
+${HOME}/MirrorKAN.munich/all-RemoteTech_pre.sh
 ${HOME}/MirrorKAN.munich/all-SCANsat_pre.sh
 ${HOME}/MirrorKAN.munich/all-Trajectories_pre.sh
 
