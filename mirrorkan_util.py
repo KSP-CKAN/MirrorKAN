976e85b5f3b1a95bdcdb9ba5184a7c01ff027845
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-01-10 01:48:45 +0200

    small fix

--- mirrorkan_util.py
+++ mirrorkan_util.py
@@ -1,6 +1,6 @@
 import os
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 
 def zipdir(path, zip):
     for root, dirs, files in os.walk(path):
@@ -10,7 +10,7 @@ def zipdir(path, zip):
 def download_file(url, path, filename):
     os.system('wget -O "' + os.path.join(path, filename) + '" "' + url + '"')
     
-def find_files_with_extension(directory, extension):
+def find_files_with_extension(directory, extension, recursive = True):
     result_list = []
     
     file_list = [ f for f in listdir(directory) if isfile(join(directory, f)) ]
@@ -20,5 +20,10 @@ def find_files_with_extension(directory, extension):
         fileName, fileExtension = os.path.splitext(f)
         if fileExtension == extension:
             result_list += [os.path.join(directory, f)]
+    
+    if recursive == True:
+        directory_list = [ f for f in listdir(directory) if isdir(join(directory, f)) ]
+        for d in directory_list:
+            result_list += find_files_with_extension(os.path.join(directory, d), extension, True)
     
     return result_list
